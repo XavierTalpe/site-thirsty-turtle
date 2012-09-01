c826b93e87f24b5e40723f6c8af15bc7a4d55ddf
Xavier Talpe <xaviertalpe@gmail.com>
2012-09-01 20:11:09 +0200

    Updated rendering of youtube videos.

--- _plugins/youtube.rb
+++ _plugins/youtube.rb
@@ -1,26 +1,18 @@
 class YouTube < Liquid::Tag
-  Syntax = /^\s*([^\s]+)(\s+(\d+)\s+(\d+)\s*)?/
+  Syntax = /^\s*([^\s]+)?/
 
   def initialize(tagName, markup, tokens)
     super
 
     if markup =~ Syntax then
       @id = $1
-
-      if $2.nil? then
-          @width = 560
-          @height = 420
-      else
-          @width = $2.to_i
-          @height = $3.to_i
-      end
     else
-      raise "No YouTube ID provided in the \"youtube\" tag"
+      raise "No YouTube ID provided in the \"youtube\" tag."
     end
   end
 
   def render(context)
-    "<iframe width=\"#{@width}\" height=\"#{@height}\" src=\"http://www.youtube.com/embed/#{@id}\" frameborder=\"0\" allowfullscreen></iframe>"
+    "<iframe width=\"565\" height=\"424\" src=\"http://www.youtube.com/embed/#{@id}\" frameborder=\"0\" allowfullscreen></iframe>"
   end
 
   Liquid::Template.register_tag "youtube", self
--- _posts/2012-08-12-hello-world.md
+++ _posts/2012-08-12-hello-world.md
@@ -24,7 +24,7 @@ Your bones don't break, mine do. That's clear. Your cells react to bacteria and 
 Well, the way they make shows is, they make one show. That show's called a pilot. Then they show that show to the people who make shows, and on the strength of that one show they decide if they're going to make more shows. Some pilots get picked and become television programs. Some don't, become nothing. She starred in one of the ones that became nothing.
 
 
-{% youtube oHg5SJYRHA0 500 400 %}
+{% youtube oHg5SJYRHA0 %}
 
 {% highlight ruby %}
 module Jekyll
