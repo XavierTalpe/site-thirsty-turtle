8bede4b466f1682b02e57e8737ec78b75281e3a1
Xavier Talpe <xaviertalpe@gmail.com>
2012-09-08 22:48:30 +0200

    Thumbnails now have preset sizes.

--- _plugins/thumbnail.rb
+++ _plugins/thumbnail.rb
@@ -5,11 +5,18 @@ class Thumbnail < Liquid::Tag
   def initialize(tag_name, markup, tokens)
     super
 
-    if markup =~ /^\s*\[([\s\w-]+)\]\[([\w\/\.-]+)\]\[([\d]+)x([\d]+)\]\s*$/
+    if markup =~ /\s*\[([\s\w-]+)\]\[([\w\/\.-]+)\](\[([a-z]+)\]|\[([\d]+)+x([\d]+)\])?\s*$/
       @alt = $1
       @src = $2
-      @width = $3
-      @height = $4
+
+      if not $5.nil? and not $6.nil?
+        @width = $5
+        @height = $6
+      elsif not $4.nil?
+        @config = $4
+      else
+        @config = 'default'
+      end
     else
       error_msg = 'Invalid arguments for thumbnail generation!'
       puts error_msg
@@ -18,6 +25,10 @@ class Thumbnail < Liquid::Tag
   end
 
   def render(context)
+    if @width.nil? or @height.nil?
+      retrieve_dimensions @config
+    end
+
     src_thumb = get_thumb_src(@width, @height)
 
     absolute_img = File.join(Dir.pwd, @src)
@@ -31,7 +42,17 @@ class Thumbnail < Liquid::Tag
       thumb.write absolute_thumb
     end
 
-    "<a class='fancybox' href='#@src'><img alt='#@alt' src='#{src_thumb}' /></a>"
+    "<a class='fancybox' href='#@src' title='#@alt'><img alt='#@alt' src='#{src_thumb}' /></a>"
+  end
+
+  def retrieve_dimensions(config)
+    if config == 'default'
+      @width = 500
+      @height = 500
+    elsif config == 'small'
+      @width = 250
+      @height = 250
+    end
   end
 
   def get_thumb_src(width, height)
