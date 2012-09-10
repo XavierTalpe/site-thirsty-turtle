3e4d61438ae18f849bb4d95bb61519b612e84159
Xavier Talpe <xaviertalpe@gmail.com>
2012-09-10 22:53:40 +0200

    Removed obsolete code.

--- _plugins/pygments.rb
+++ _plugins/pygments.rb
@@ -22,16 +22,9 @@ module Jekyll
 
     def render(context)
       request = Net::HTTP.post_form(WEB_APP_URL, {'lang' => @lang, 'code' => super})
+      code = request.body
 
-      output = add_code_tags(request.body, @lang)
-      output = context["pygments_prefix"] + output if context["pygments_prefix"]
-      output = output + context["pygments_suffix"] if context["pygments_suffix"]
-      output
-    end
-
-    def add_code_tags(code, lang)
-      # Add nested <code> tags to code blocks
-      code = code.sub(/<pre>/, '<pre><code class="' + lang + '">')
+      code = code.sub(/<pre>/, '<pre><code class="' + @lang + '">')
       code = code.sub(/<\/pre>/, "</code></pre>")
     end
 
